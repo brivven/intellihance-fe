c97efee7166820b45daea3e07f9e448cc5f5ab8e
brivven <abdoumadrid610@gmail.com>
2023-03-09 02:51:38 +0100

    g

--- components/SplashScreen.js
+++ components/SplashScreen.js
@@ -76,11 +76,11 @@ function SplashScreen({finishedLoading, showWebsite}) {
       <Flex width="fit-content" align="center" justify={'center'} pb={5} className='logo-container' >
 
         <Flex bgColor="transparent" align="center" justify={'center'} flexDirection='row' h='fit-content'>
-          <Image src="/intellihance.png" alt="intellihance Logo" h={{base:"35px", sm:"45px", md:"50px" }} className='intellihance-logo'  />
+          <Image src="/intellihance.png" alt="intellihance Logo" h={{base:"40px", sm:"45px", md:"50px" }} className='intellihance-logo'  />
         </Flex>
 
         <Text letterSpacing={'1px'} px={1} bgColor="transparent" className='intellihance' 
-          fontSize={{base:"25px", sm:"35px", md:"45px" }} color='#39C7C4' fontWeight={500} userSelect='none'
+          fontSize={{base:"35px", sm:"40px", md:"45px" }} color='#39C7C4' fontWeight={500} userSelect='none'
         >  
           {intellihanceText.split("").map(function(char, index){
             return <span style={{display:'inline-block', fontFamily:'Chewy'}} key={index}>{char}</span>;
